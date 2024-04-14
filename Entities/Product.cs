7c5166eba578173ccc997a1ce979f1ca1d9aec02
Kamranbb <kamrannb@code.edu.az>
2024-04-14 19:55:45 +0400

    Proje dosyası ekle.

---
+++ Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace AbtractClassTask.Entities
+{
+    internal class Product
+    {
+        public  string Name { get; set; }
+        public  double   Price { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name}, Price:{Price}, Count:{Count} ";
+        }
+
+        public void Discount(int discount)
+        {
+            Console.WriteLine(Price - (Price * discount) / 100);
+
+        }
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AbtractClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
